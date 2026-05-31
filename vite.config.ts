15fcc8abb3ff237bea63f1938d413fbe2f52cdb0
lelechen-0815 <yuhanp1110@gmail.com>
2026-05-31 18:47:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.png'],
       manifest: {
         name: '自嘲熊记账',
         short_name: '🐻记账',
